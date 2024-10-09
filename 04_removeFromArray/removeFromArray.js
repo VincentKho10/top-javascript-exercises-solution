2ade0827a65906b0160a2b0ab9dc33507d0ecc05
pembe <pembe@ADMIN8PEMBELIAN>
2024-10-09 15:57:40 +0700

    sanitize list into unique, find and delete InProgress

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, find) {
+    const arr = [...new Set(list)]
+    console.log(find)
+    const del_index = arr.findIndex((v,i)=>v!=find);
+    console.log(arr.splice(del_index,1))
+    return arr.splice(del_index,1);
 };
 
 // Do not edit below this line
