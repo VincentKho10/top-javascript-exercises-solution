982f3b09be866dab5921909be037c72a0c6fc3cb
pembe <pembe@ADMIN8PEMBELIAN>
2024-10-09 15:28:19 +0700

    02_repeatString Done \n03_reverseString Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
 const repeatString = function(word, repeat_by) {
-    return word.repeat(repeat_by)
+    if (repeat_by>=0){
+        return word.repeat(repeat_by)
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const wl = [...word]
+    res = wl.reduceRight((ac,cur) => ac+cur, '')
+    return res
 };
 
 // Do not edit below this line
