0493807bb84371bf7f09d4ff25c5a72cf5229635
VincentKho10 <38545087+VincentKho10@users.noreply.github.com>
2024-10-10 19:48:43 +0700

    palindrome initial test Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const lastidx = text.length-1
+    const iter = Math.floor(text.length/2)
+    const sanitized = text.replace(' ','')
+    for(let i=0; i<iter; i++){
+        console.log(sanitized[i])
+        console.log(sanitized[lastidx-i])
+        if(sanitized[i]!=sanitized[lastidx-i]){
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
