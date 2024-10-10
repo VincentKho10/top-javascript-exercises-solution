f8c69166d1d10f93f1b009a26c0acac1ef686e78
pembe <pembe@ADMIN8PEMBELIAN>
2024-10-10 15:00:30 +0700

    sumAll Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,9 @@
 const sumAll = function(o1,o2) {
+    const isValid = o1+o2
+    const isInt = Number.isInteger(isValid)
+    if(!isInt || o1*o2 < 0){
+        return "ERROR"
+    }
     const half_count = (Math.abs(o1-o2)+1)/2
     const sum_minmax = o1+o2
     return half_count * sum_minmax
