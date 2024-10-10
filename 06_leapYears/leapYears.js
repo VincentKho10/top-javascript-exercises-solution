3c5b3c34fccd905cb974ad547268ef2df1af4067
pembe <pembe@ADMIN8PEMBELIAN>
2024-10-10 15:06:41 +0700

    leapYear initial test Done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // leap year divisible 4
+    // not divisible 100
+    // except 400
+    return (year%4==0 && year%100>0)||year%400==0
 };
 
 // Do not edit below this line
