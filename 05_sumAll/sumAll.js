708695a113ebb7db9a001704508e5b343c351da6
pembe <pembe@ADMIN8PEMBELIAN>
2024-10-10 14:45:20 +0700

    sumAll initial test Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function(o1,o2) {
+    const half_count = (Math.abs(o1-o2)+1)/2
+    const sum_minmax = o1+o2
+    return half_count * sum_minmax
 };
 
 // Do not edit below this line
