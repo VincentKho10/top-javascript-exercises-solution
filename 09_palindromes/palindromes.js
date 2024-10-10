b1f8332333d7fda1e4d7d1f4f1d269150e97f3b4
VincentKho10 <38545087+VincentKho10@users.noreply.github.com>
2024-10-10 20:04:01 +0700

    palindrome Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,10 +1,10 @@
 const palindromes = function (text) {
-    const lastidx = text.length-1
-    const iter = Math.floor(text.length/2)
-    const sanitized = text.replace(' ','')
+    let sanitized = text.replace(/[^a-zA-Z0-9]/g,'')
+    sanitized = sanitized.toLowerCase()
+    const lastidx = sanitized.length-1
+    const iter = Math.floor(sanitized.length/2)
+    console.log(sanitized)
     for(let i=0; i<iter; i++){
-        console.log(sanitized[i])
-        console.log(sanitized[lastidx-i])
         if(sanitized[i]!=sanitized[lastidx-i]){
             return false
         }
