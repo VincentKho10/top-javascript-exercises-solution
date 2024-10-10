773d168b34e3000be2836406d5504b95a95f14d7
pembe <pembe@ADMIN8PEMBELIAN>
2024-10-10 15:51:27 +0700

    tempConversion Done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let rounded = parseFloat(Math.round((5/9).toFixed(4)*10000)/10000)
+  return parseFloat(((farenheit-32)*rounded).toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let rounded = parseFloat(Math.round((9/5).toFixed(2)*100)/100)
+  return parseFloat((celcius*rounded+32).toFixed(1))
 };
 
 // Do not edit below this line
