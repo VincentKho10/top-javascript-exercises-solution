bca85c6e3ba6ca56880ebd7e48b703e3ca0dc980
VincentKho10 <38545087+VincentKho10@users.noreply.github.com>
2024-10-10 19:25:17 +0700

    calculator initial test Done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(o1,o2) {
+	return o1+o2
 };
 
-const subtract = function() {
-	
+const subtract = function(o1,o2) {
+	return o1-o2
 };
 
-const sum = function() {
-	
+const sum = function(o) {
+	return o.reduce((acc,curr)=>acc+curr,0)
 };
 
-const multiply = function() {
-
+const multiply = function(o1,o2) {
+  return o1*o2
 };
 
-const power = function() {
-	
+const power = function(o1,o2) {
+	return Math.pow(o1,o2)
 };
 
-const factorial = function() {
-	
+const factorial = function(o) {
+	temp = o
+  res=1
+  while(temp>0){
+    res*=temp
+    temp-=1
+  }
+  return res
 };
 
 // Do not edit below this line
