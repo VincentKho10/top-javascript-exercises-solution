95bbaa69b3d9b4b197d69659c8c7d38ff2bae123
VincentKho10 <38545087+VincentKho10@users.noreply.github.com>
2024-10-12 10:51:07 +0700

    findTheOldest Done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,15 +1,27 @@
-const findTheOldest = function(arr_person) {
-    const oldst = arr_person.reduce((acc,curr)=>{
-        const age = curr.yearOfDeath - curr.yearOfBirth
-        if (acc<age){
-            return age
-        }
-        return acc
-    },0)
-    return arr_person.find((v)=>{
-        const age = v.yearOfDeath - v.yearOfBirth
-        return age == oldst        
-    })
+const findTheOldest = function (arr_person) {
+  const oldst = arr_person.reduce(
+    (acc, curr) => {
+      const curryod = curr.yearOfDeath
+        ? curr.yearOfDeath
+        : new Date().getFullYear();
+      const currage = curryod - curr.yearOfBirth;
+      const accyod = acc.yearOfDeath
+        ? acc.yearOfDeath
+        : new Date().getFullYear();
+      const accage = accyod - acc.yearOfBirth;
+
+      if (accage < currage) {
+        return curr;
+      }
+      return acc;
+    },
+    { yearOfDeath: 1, yearOfBirth: 1 }
+  );
+
+  return arr_person.find((v) => {
+    // console.log(v)
+    return v === oldst;
+  });
 };
 
 // Do not edit below this line
